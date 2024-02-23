31ada0150acbc8e4082878c13cf6d698213b31b2
Ljupcho Nakov <nakovabv@gmail.com>
2024-02-23 21:40:25 +0100

    rollback

--- app.js
+++ app.js
@@ -41,129 +41,47 @@ const uploadAttachmentAndGetToken = async (attachment) => {
 // Use CSS Style
 app.use("/",express.static(__dirname + "/"));
 
+
 // Use body-parser middleware to parse URL-encoded data
+app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
 // Render the form with reCAPTCHA
-// app.get("/", (req, res) => {
-//   res.sendFile(__dirname + "/index.html");
-// });
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
 
+/*
 app.get("/", recaptcha.middleware.render, (req, res) => {
   const form = `
-  <style>
-  body {
-    font-family: Arial, sans-serif;
-    margin: 0;
-  }
-
-  form {
-    max-width: 500px;
-    margin: 0 auto;
-  }
-
-  label {
-    display: block;
-    margin-bottom: 10px;
-  }
-
-  input[type="text"],
-  input[type="email"],
-  input[type="file"],
-  textarea {
-    width: 100%;
-    padding: 8px;
-    margin-bottom: 10px;
-    border: 1px solid #ccc;
-    border-radius: 4px;
-    box-sizing: border-box;
-  }
-
-  button {
-    background-color: #4caf50;
-    color: white;
-    padding: 10px 20px;
-    border: none;
-    border-radius: 4px;
-    cursor: pointer;
-  }
-
-  button:hover {
-    background-color: #45a049;
-  }
-
-  input.form-control.contact-form {
-    height: 38px;
-    border: 1px solid #231f20;
-    border-radius: 6px;
-    font-size: 14px;
-    margin-bottom: 16px;
-    padding: 6px 12px;
-  }
-
-  textarea.form-control.contact-form {
-    height: auto;
-    border: 1px solid #231f20;
-    border-radius: 6px;
-    font-size: 14px;
-    padding: 6px 12px;
-  }
-
-  .form-control:focus {
-    box-shadow: none;
-    -webkit-box-shadow: none;
-    color: #495057;
-    background-color: #fff;
-    border-color: #6d6348;
-    outline: 0;
-  }
-
-  input.form-control.contact-form::placeholder,
-  textarea.form-control.contact-form::placeholder {
-    font-size: 18px !important;
-    font-weight: 500 !important;
-    color: darkgrey !important;
-  }
-
-  button.btn.btn-lg.btn-default.btn-contact-form {
-    background-color: #a81d4d;
-    color: white;
-    padding: 15px 55px;
-    border-radius: 6px;
-    font-size: 21px;
-    font-weight: 400;
-    margin: 16px auto;
-  }
-
-  </style>
-    <form id="supportForm" action="/submit" method="post" enctype="multipart/form-data" style="width: 100%; max-width: max-content;">
-        <div>
-            <input class="form-control contact-form" type="text" name="subject" placeholder="Subject*" required>
-            <input class="form-control contact-form" type="text" name="name" placeholder="Your Name*" required>
-            <input class="form-control contact-form" type="email" name="email" placeholder="Your Email*" required>
-            <input class="form-control contact-form" type="text" name="phone" placeholder="Contact Phone Number*"
-                required>
-            <input class="form-control contact-form" type="text" name="order" placeholder="Order Number*" required>
-            <input class="form-control contact-form" type="text" name="sku" placeholder="Product SKU*" required>
-            <textarea class="form-control contact-form" name="description" rows="6" placeholder="Your Message*"
-                required></textarea>
-            <label style="font-weight: 600;" for="attachment">File Upload</label>
-            <input type="file" name="attachment" accept="image/*" multiple>
-        </div>
-        <div>
+    <form action="/submit" method="post" enctype="multipart/form-data">
+    <div>
+        <input type="text" name="subject" placeholder="Subject*" required><br>
+        <input type="text" name="name" placeholder="Your Name*" required><br>
+        <input type="email" name="email" placeholder="Your Email*" required><br>
+        <input type="text" name="phone" placeholder="Contact Phone Number*" required><br>
+        <input type="text" name="order" placeholder="Order Number*" required><br>
+        <input type="text" name="sku" placeholder="Product SKU*" required><br>
+        <textarea name="description" rows="6" placeholder="Your Message*" required></textarea><br>
+        <label for="attachment">File Upload</label><br>
+        <input type="file" name="attachment" accept="image/*" multiple><br><br>
+    </div>
+    <div>
         ${recaptcha.render()} <!-- Render reCAPTCHA widget -->
-        </div>
-            <button class="btn btn-lg btn-default btn-contact-form" type="submit">Send Support Ticket</button>
-        </div>
+    </div>
+    <div>
+        <button>Submit</button>
+    </div>
     </form>
     `;
   res.send(form);
 });
-
+*/
 
 // Handle form submission
 app.post("/submit", recaptcha.middleware.verify, async (req, res) => {
+  console.log(req.recaptcha);
   if (!req.recaptcha.error) {
     // If reCAPTCHA verification is successful
     try {
@@ -171,6 +89,7 @@ app.post("/submit", recaptcha.middleware.verify, async (req, res) => {
       form.options.allowEmptyFiles = true; // Allow files with size 0
       form.options.minFileSize = 0; // Allow files with size 0
       form.parse(req, async (err, fields, files) => {
+        console.log(fields);
         if (err) {
           // Handle form parsing error
           console.error("Error parsing form:", err);
